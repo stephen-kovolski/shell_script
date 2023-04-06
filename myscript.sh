952f630316fb5743b3c2c5c9428a5f6d00c205fd
stephen-kovolski <s.kovolski@gmail.com>
2023-04-05 22:51:07 -0400

    initial commit

---
+++ myscript.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/bash
+
+# ECHO COMMAND
+#echo Hello World!
+
+# VARIABLES 
+# ALWAYS UPPERCASE
+# letters numbers and underscores
+
+NAME="Bob"
+#echo "My name is $NAME"
+
+# USER INPUT
+#read -p "Enter your name: " NAME
+#echo "Hello $NAME, nice to meet you!"
+
+# SIMPLE IF STATEMENTS
+#if [ "$NAME" == "STEVE" ]
+#then
+#    echo "Your name is Steve"
+#fi
+
+# IF-ELSE
+#if [ "$NAME" == "STEVE" ]
+#then
+#    echo "Your name is Steve"
+#else
+#    echo "Your name is not Steve"
+#fi
+
+
+# ELSE-IF (elif)
+#if [ "$NAME" == "STEVE" ]
+#then
+#    echo "Your name is Steve"
+#elif [ "$NAME" == "Joe" ]
+#then
+#    echo "Your name is Joe"
+#else
+#    echo "Your name is not Steve or Joe"
+#fi
